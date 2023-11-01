b03c781cbd13f0b0850ae126014ce6036f396e0c
Franco Veron Peralta <francoveronperalta@gmail.com>
2023-11-01 10:25:37 -0300

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import CustomerReviews from "./CustomerReviews"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffers from './SpecialOffers'
+import SuperQuality from './SuperQuality'
+import Suscribe from './Suscribe'
+
+export {CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffers, SuperQuality, Suscribe  }
